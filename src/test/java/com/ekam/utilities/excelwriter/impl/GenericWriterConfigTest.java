58f537551906d49aebeb79f4791379a27673b815
Aashay Saralkar <4965974+aashaysaralkar@users.noreply.github.com>
2017-11-10 19:05:57 +0530

    Add files via upload
    Test case for generating sample Excel using data from resources/airports.json

---
+++ src/test/java/com/ekam/utilities/excelwriter/impl/GenericWriterConfigTest.java
@@ -0,0 +1,97 @@
+package com.ekam.utilities.excelwriter.impl;
+
+import org.junit.Test;
+
+import com.ekam.utilities.excelwriter.base.HeaderWriter;
+import com.ekam.utilities.excelwriter.base.SheetDataWriter;
+import com.ekam.utilities.excelwriter.base.WriteConfig;
+
+public class GenericWriterConfigTest {
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void throwsExceptionIfWriteConfigIsEmpty() throws Exception{
+		
+		TestHeaderWriter headerWriter = new TestHeaderWriter();
+		TestDataWriter dataWriter = new TestDataWriter();
+		GenericFileWriter genericFileWriter = new GenericFileWriter(headerWriter,dataWriter);
+		WriteConfig config = new WriteConfig();
+		genericFileWriter.write(config);
+		genericFileWriter.close();
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void throwsExceptionIfFileLocationIsEmpty() throws Exception{
+		
+		TestHeaderWriter headerWriter = new TestHeaderWriter();
+		TestDataWriter dataWriter = new TestDataWriter();
+		GenericFileWriter genericFileWriter = new GenericFileWriter(headerWriter,dataWriter);
+		WriteConfig config = new WriteConfig();
+		genericFileWriter.write(config);
+		genericFileWriter.close();
+	}
+	
+	
+	
+	
+	
+	class TestHeaderWriter implements  HeaderWriter{
+
+		public void init(WriteConfig writeConfig) {
+			
+		}
+
+		public void write(WriteConfig writeConfig) {
+			
+		}
+
+		public void close() {
+			
+		}
+		
+	}
+	
+	class TestDataWriter implements SheetDataWriter{
+
+		public void init(WriteConfig writerContext) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public int totalSheetsToWrite() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		public void beforeFileWritingStart() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void beforeSheetWritingStart() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void writeSheetData(WriteConfig writeConfig) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void afterSheetWritingEnd() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void afterFileWritingEnd() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void close() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+
+}
--- src/test/java/com/ekam/utilities/excelwriter/impl/GenericWriterTests.java
+++ src/test/java/com/ekam/utilities/excelwriter/impl/GenericWriterTests.java
@@ -1,97 +1,253 @@
 package com.ekam.utilities.excelwriter.impl;
 
+import java.io.InputStreamReader;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.junit.Assert;
 import org.junit.Test;
 
-import com.ekam.utilities.excelwriter.base.HeaderWriter;
 import com.ekam.utilities.excelwriter.base.SheetDataWriter;
 import com.ekam.utilities.excelwriter.base.WriteConfig;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 
 public class GenericWriterTests {
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void throwsExceptionIfWriteConfigIsEmpty() throws Exception{
-		
-		TestHeaderWriter headerWriter = new TestHeaderWriter();
-		TestDataWriter dataWriter = new TestDataWriter();
-		GenericFileWriter genericFileWriter = new GenericFileWriter(headerWriter,dataWriter);
-		WriteConfig config = new WriteConfig();
-		genericFileWriter.write(config);
-		genericFileWriter.close();
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void throwsExceptionIfFileLocationIsEmpty() throws Exception{
-		
-		TestHeaderWriter headerWriter = new TestHeaderWriter();
-		TestDataWriter dataWriter = new TestDataWriter();
-		GenericFileWriter genericFileWriter = new GenericFileWriter(headerWriter,dataWriter);
-		WriteConfig config = new WriteConfig();
-		genericFileWriter.write(config);
-		genericFileWriter.close();
+
+	private static final String[] HEADERS = "Name,Type, Continent, Size,Status".split(",");
+	@Test
+	public void test() {
+		try {
+			TestHeaderWriter headerWriter = new TestHeaderWriter();
+			
+			TestDataWriter dataWriter = new TestDataWriter();
+			GenericFileWriter genericFileWriter = new GenericFileWriter(headerWriter,dataWriter);
+			WriteConfig config = new WriteConfig();
+			config.put(WriteConfig.CONFIG_OUTPUT_FILE_NAME, "airports.xlsx");
+			config.put(WriteConfig.CONFIG_OUTPUT_FILE_LOCATION, "D:\\ExcelWriter");
+			genericFileWriter.write(config);
+			genericFileWriter.close();
+		} catch (Exception e) {
+			Assert.fail("Excel generation failed due to "+e);
+		}
 	}
 	
 	
 	
-	
-	
-	class TestHeaderWriter implements  HeaderWriter{
+	class TestHeaderWriter extends ArrayBasedHeaderWriter{
 
 		public void init(WriteConfig writeConfig) {
-			
-		}
-
-		public void write(WriteConfig writeConfig) {
+			// Nothing as of now
 			
 		}
 
 		public void close() {
+			//Nothing as of now
 			
 		}
+
+		@Override
+		public String[] getHeaders() {
+			return HEADERS;
+		}
+
+		
 		
 	}
 	
 	class TestDataWriter implements SheetDataWriter{
+		
+		JsonReader reader;
+		Gson gson;
+		CellStyle airportStyle;
+		CellStyle helicoptorStyle;
 
-		public void init(WriteConfig writerContext) {
-			// TODO Auto-generated method stub
+		public void init(WriteConfig writerContext){
+			reader = new JsonReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("airports.json")));
+			reader.setLenient(Boolean.TRUE);
+			gson = new Gson();
+			Workbook workbook = writerContext.get(WriteConfig.CONFIG_WORKBOOK, Workbook.class);
+	        Font headerFont = workbook.createFont();
+	        headerFont.setBold(true);
+	        airportStyle = createBorderedStyle(workbook);
+	        airportStyle.setAlignment(HorizontalAlignment.CENTER);
+	        airportStyle.setFillForegroundColor(IndexedColors.LIGHT_CORNFLOWER_BLUE.getIndex());
+	        airportStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+	        airportStyle.setFont(headerFont);
+
+	        helicoptorStyle = createBorderedStyle(workbook);
+	        helicoptorStyle.setAlignment(HorizontalAlignment.CENTER);
+	        helicoptorStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+	        helicoptorStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+	        helicoptorStyle.setFont(headerFont);
 			
 		}
 
 		public int totalSheetsToWrite() {
-			// TODO Auto-generated method stub
-			return 0;
+			return 1;
 		}
 
 		public void beforeFileWritingStart() {
-			// TODO Auto-generated method stub
+			
 			
 		}
 
 		public void beforeSheetWritingStart() {
-			// TODO Auto-generated method stub
+			// Nothing as of now
 			
 		}
 
 		public void writeSheetData(WriteConfig writeConfig) {
-			// TODO Auto-generated method stub
+			try {
+				reader.beginArray();
+				SXSSFSheet sheet = writeConfig.get(WriteConfig.CONFIG_CURRENT_SHEET, SXSSFSheet.class);
+				RowCellCounter rowCellCounter = writeConfig.get(WriteConfig.CONFIG_CURRENT_ROW_CELL_COUNTER, RowCellCounter.class);
+				sheet.trackColumnForAutoSizing(0);
+				sheet.trackColumnForAutoSizing(1);
+				while (reader.hasNext()) {
+					Airport airport = gson.fromJson(reader, Airport.class);
+					if(StringUtils.isNoneEmpty(airport.getName())){
+						Row row = sheet.createRow(rowCellCounter.nextRowNum());
+						
+						Cell cell = row.createCell(rowCellCounter.nextCellNum());
+						cell.setCellValue(airport.getName());
+						cell.setCellStyle("airport".equals(airport.getType()) ? airportStyle : helicoptorStyle);
+						
+						cell = row.createCell(rowCellCounter.nextCellNum());
+						cell.setCellValue(airport.getType());			
+						
+						cell = row.createCell(rowCellCounter.nextCellNum());
+						cell.setCellValue(airport.getContinent());	
+						
+						cell = row.createCell(rowCellCounter.nextCellNum());
+						cell.setCellValue(StringUtils.isEmpty(airport.getSize()) ? "NA":airport.getSize());					
+						
+						cell = row.createCell(rowCellCounter.nextCellNum());
+						cell.setCellValue(StringUtils.equals(airport.getStatus(),"1") ? "Active" : "Inactive");
+						
+					}
+				}
+				sheet.autoSizeColumn(0);
+				sheet.autoSizeColumn(1);
+				reader.endArray();
+				reader.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			
 		}
 
 		public void afterSheetWritingEnd() {
-			// TODO Auto-generated method stub
+			// Nothing as of now
 			
 		}
 
 		public void afterFileWritingEnd() {
-			// TODO Auto-generated method stub
+			// Nothing as of now
 			
 		}
 
 		public void close() {
-			// TODO Auto-generated method stub
-			
+			// Nothing as of now
+		}
+		
+		private CellStyle createBorderedStyle(Workbook wb){
+	        BorderStyle thin = BorderStyle.THIN;
+	        short black = IndexedColors.BLACK.getIndex();
+	        
+	        CellStyle style = wb.createCellStyle();
+	        style.setBorderRight(thin);
+	        style.setRightBorderColor(black);
+	        style.setBorderBottom(thin);
+	        style.setBottomBorderColor(black);
+	        style.setBorderLeft(thin);
+	        style.setLeftBorderColor(black);
+	        style.setBorderTop(thin);
+	        style.setTopBorderColor(black);
+	        return style;
+	    }
+		
+	}
+	
+	@SuppressWarnings("unused")
+	private class Airport{
+		private String name;
+		private String type;
+		private String continent;
+		private String size;
+		private String status;
+		/**
+		 * @return the name
+		 */
+		public final String getName() {
+			return name;
+		}
+		/**
+		 * @param name the name to set
+		 */
+		public final void setName(String name) {
+			this.name = name;
+		}
+		/**
+		 * @return the type
+		 */
+		public final String getType() {
+			return type;
+		}
+		/**
+		 * @param type the type to set
+		 */
+		public final void setType(String type) {
+			this.type = type;
+		}
+		/**
+		 * @return the continent
+		 */
+		public final String getContinent() {
+			return continent;
+		}
+		/**
+		 * @param continent the continent to set
+		 */
+		public final void setContinent(String continent) {
+			this.continent = continent;
+		}
+		/**
+		 * @return the size
+		 */
+		public final String getSize() {
+			return size;
+		}
+		/**
+		 * @param size the size to set
+		 */
+		public final void setSize(String size) {
+			this.size = size;
+		}
+		/**
+		 * @return the status
+		 */
+		public final String getStatus() {
+			return status;
+		}
+		/**
+		 * @param status the status to set
+		 */
+		public final void setStatus(String status) {
+			this.status = status;
 		}
 		
 	}
 
 }
+
+
